c0a968e86e2976d6dc88b9046e1a719d12cc915a
oyatillo0509 <tilloxon0509@gmail.com>
2024-08-28 13:52:57 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form from './components/Form'
+
+function App() {
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>
+      <Form />
+    </div>
+  )
+}
+
+export default App
